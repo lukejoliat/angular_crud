e8500cf268d99a1d1be6a91f7d10799f578b8605
Luke Joliat <lukejoliat@gmail.com>
2016-11-12 00:36:40 -0800

    this is the config file

---
+++ config/product.php
@@ -0,0 +1,50 @@
+<?php
+
+class Product {
+  
+  private $conn;
+  private $table_name = 'products';
+  
+  public $id;
+  public $name;
+  public $description;
+  public $price;
+  public $created;
+  
+  public function __construct($db) {
+    $this->conn = $db;
+  }
+  
+  function readAll() {
+    $query = "SELECT id, name, description, price, created FROM " . $this->table_name . " ORDER BY id DESC";
+    $stmt = $this->conn->prepare( $query );
+    $stmt->execute();
+    return $stmt;
+  }
+  
+  function readOne() {
+    $query = "SELECT id, name, description, price, created FROM " . $this->table_name . " WHERE id = " .  $this->id . "";
+    $stmt = $this->conn->prepare( $query );
+    $stmt->execute();
+    return $stmt;
+  }
+  
+  function deleteProduct() {
+    $query = "DELETE FROM ". $this->table_name ." WHERE id = " . $this->id . "";
+    $stmt = $this->conn->prepare( $query );
+    $stmt->execute();
+    return $stmt;
+  }
+  
+  function createProduct() {
+    $query = "INSERT INTO " . $this->table_name . "(name, description, price) VALUES (:name, :description, :price)";
+    $stmt = $this->conn->prepare( $query );
+    $stmt->bindParam(':name', $this->name, PDO::PARAM_STR, 100);
+    $stmt->bindParam(':description', $this->description, PDO::PARAM_STR, 100);
+    $stmt->bindParam(':price', $this->price, PDO::PARAM_STR, 100);
+    $stmt->execute();
+    return $stmt;
+  }  
+}
+
+?>
